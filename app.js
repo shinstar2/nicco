821b96b244ef40658d5d8705dcd951d185217975
SHINBYEOLYI <151127118+SHINBYEOLYI@users.noreply.github.com>
2023-11-29 00:54:55 +0900

    Add First Quest.

---
+++ app.js
@@ -0,0 +1,15 @@
+
+let a = 4 ; 
+const b = 5 ; 
+let myName = "star"
+/**
+ * a 의 값을 4로 변수 할당하고, 추후에 다른 숫자로 바꾸고 싶을 때 
+ * 콘솔로그 안의 숫자를 바꾸는 대신 const a = 원하는 숫자로 바꿔주면 됨
+ */
+console.log(a + b);
+console.log(a / b);
+console.log(a * b);
+console.log("hello " + myName)
+
+myName = "shinbyeolyi"
+console.log(myName);
